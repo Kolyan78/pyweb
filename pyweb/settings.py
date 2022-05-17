754d925577c66a5744ac854919141c4f2d8b8580
kolya <kolyan78@gmail.com>
2022-05-17 13:28:44 +0300

    init2

--- pyweb/settings.py
+++ pyweb/settings.py
@@ -24,12 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = 'django-insecure-gm%f^18-qz29uni9&71wf59!2g^cwb+-glwyj08j8np-de&t%3'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
 DEBUG = os.getenv('DEBUG') == 'true'
 
+#ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS').split(',')]
+
 
 
 # Application definition
